6fe805e3cdf4514f3f570d86bd24f95940cd05f1
Henrique da Silva Lisboa 22C <henrique6@eaportal.org>
2024-06-05 02:34:24 +0000

    ajustes da função de selecionar assunto para funcionar em todos os dispositivos

--- pages/resultado/resultado.js
+++ pages/resultado/resultado.js
@@ -4,7 +4,23 @@ const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
 const assunto = localStorage.getItem("assunto")
 
-cone de ${assunto}`)
+
+botaoTema.addEventListener("click", () => {
+    trocarTema(body, botaoTema)
+})
+
+botaoJogarNovamente.addEventListener("click", jogarNovamente)
+
+verificarTema(body, botaoTema)
+
+function alterarAssunto() {
+    const divIcone = document.querySelector(".assunto_icone")
+    const iconeImg = document.querySelector(".assunto_icone img")
+    const assuntoTitulo = document.querySelector(".assunto h1")
+
+    divIcone.classList.add(assunto.toLowerCase())
+    iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLowerCase()}.svg`)
+    iconeImg.setAttribute("alt", `ìcone de ${assunto}`)
     assuntoTitulo.innerText = assunto
 }
 
