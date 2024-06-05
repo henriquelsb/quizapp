4769f265847dbfe5bcf716ca12d2614b663e1635
Henrique da Silva Lisboa 22C <henrique6@eaportal.org>
2024-06-05 02:33:02 +0000

    adicionada a função de jogar novamente e FINALIZAÇÃO DO PROJETO

--- pages/resultado/resultado.js
+++ pages/resultado/resultado.js
@@ -4,23 +4,7 @@ const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
 const assunto = localStorage.getItem("assunto")
 
-
-botaoTema.addEventListener("click", () => {
-    trocarTema(body, botaoTema)
-})
-
-
-
-verificarTema(body, botaoTema)
-
-function alterarAssunto() {
-    const divIcone = document.querySelector(".assunto_icone")
-    const iconeImg = document.querySelector(".assunto_icone img")
-    const assuntoTitulo = document.querySelector(".assunto h1")
-
-    divIcone.classList.add(assunto.toLowerCase())
-    iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLowerCase()}.svg`)
-    iconeImg.setAttribute("alt", `ìcone de ${assunto}`)
+cone de ${assunto}`)
     assuntoTitulo.innerText = assunto
 }
 
