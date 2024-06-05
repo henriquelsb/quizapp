b4620ea40df8a4b5417cdeaeaa50dbbb1834d7fa
Henrique da Silva Lisboa 22C <henrique6@eaportal.org>
2024-06-05 02:25:45 +0000

    adicionada as funções de buscar as perguntas e montar as perguntas

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -8,20 +8,6 @@ let quiz = {}
 let pontos = 0
 let pergunta = 1
 let resposta = ""
-let idInputResposta = ""
-let respostaCorretaId = ""
-
-botaoTema.addEventListener("click", () => {
-    trocarTema(body, botaoTema)
-})
-
-verificarTema(body, botaoTema)
-
-function alterarAssunto() {
-    const divIcone = document.querySelector(".assunto_icone")
-    const iconeImg = document.querySelector(".assunto_icone img")
-    const assuntoTitulo = document.querySelector(".assunto h1")
-
     divIcone.classList.add(assunto.toLowerCase())
     iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLowerCase()}.svg`)
     iconeImg.setAttribute("alt", `ìcone de ${assunto}`)
